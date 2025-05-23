c4879757f53b4e8dfbfd5fcd53b384193f57ad6f
harshita1178 <usekarlomujhe@gmail.com>
2025-05-23 10:12:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-accountID = config.getint('Credentials', '8043832960')
-accountHash = config.get('Credentials', '12efef2ba448d268459dc136427d1ba0')
-Token = config.get('Credentials', '8064617516:AAFHdzKwdJ39rOhPXuKeePW2yVJ8gP8kJTw')
-DebugId=config.get('Credentials', '8043832960')
+
+accountID = config.getint('Credentials', 'accountID')
+accountHash = config.get('Credentials', 'accountHash')
+Token = config.get('Credentials', 'Token')
+DebugId = config.get('Credentials', 'DebugId')
